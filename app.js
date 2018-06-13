cfa43edc1e1138091b58691591a9593595ca2a6d
cvbutani <chirag.butani91@gmail.com>
2018-06-13 02:23:56 +0000

    Add new comment using button

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     Campground  = require("./models/campground"),
-    seedDB      = require("./seeds");
+    seedDB      = require("./seeds"),
+    Comment     = require("./models/comment");
     
 seedDB();
 
@@ -12,34 +13,53 @@ mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//  ===============
+//  HOME PAGE ROUTE
+//  ===============
 app.get("/", function(req, res) {
          res.render("landing");
 });
+
+//  ===========
+//  INDEX ROUTE
+//  ===========
 
 app.get("/campgrounds", function(req, res) {
     Campground.find({}, function(err, camps) {
         if(err) {
             console.log(err);
         } else {
-            res.render("index", {campgrounds:camps});
+            res.render("campgrounds/index", {campgrounds:camps});
         }
     });
    
 });
 
+//  ====================
+//  NEW CAMPGROUND ROUTE
+//  ====================
+
 app.get("/campgrounds/new", function(req, res) {
-    res.render("new.ejs");
+    res.render("campgrounds/new.ejs");
 });
+
+//  =====================
+//  SHOW CAMPGROUND ROUTE
+//  =====================
 
 app.get("/campgrounds/:id", function(req, res) {
     Campground.findById(req.params.id).populate("comments").exec(function(err,findCamp){
         if(err){
             console.log(err);
         } else {
-            res.render("show", {campground:findCamp});
+            res.render("campgrounds/show", {campground:findCamp});
         }
     });
 });
+
+//  =======================
+//  CREATE CAMPGROUND ROUTE
+//  =======================
 
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
@@ -56,6 +76,38 @@ app.post("/campgrounds", function(req, res) {
     });
 });
 
+//  ==============
+//  COMMENTS ROUTE
+//  ==============
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    Campground.findById(req.params.id, function(err,campground)  {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground:campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req,res){
+    Campground.findById(req.params.id, function(err,campground)  {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Connected to server");
