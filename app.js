bb3d0f6ded7d7db3719b079b7b108c68a6cef4c3
cvbutani <chirag.butani91@gmail.com>
2018-06-14 01:22:31 +0000

    Register and login logic

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    Campground  = require("./models/campground"),
-    seedDB      = require("./seeds"),
-    Comment     = require("./models/comment");
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    Campground      = require("./models/campground"),
+    seedDB          = require("./seeds"),
+    Comment         = require("./models/comment")
+    User            = require("./models/user");
     
-seedDB();
-
 mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+seedDB();
+
+//  ===============
+//  PASSPORT CONFIG
+//  ===============
+app.use(require("express-session")({
+    secret:"I have nissan altima 2016",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //  ===============
 //  HOME PAGE ROUTE
@@ -108,6 +127,43 @@ app.post("/campgrounds/:id/comments", function(req,res){
             });
         }
     });
+});
+
+//  ==========
+//  AUTH ROUTE
+//  ==========
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err,user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+//  ==========
+//  LOGIN FORM
+//  ==========
+
+app.get("/login",function(req, res) {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }),  function(req, res) {
+    res.send("LOGIN LOGIC");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
