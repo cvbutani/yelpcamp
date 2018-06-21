c15a821a9f103ba7594656bb53271b7712cd3cf9
cvbutani <chirag.butani91@gmail.com>
2018-06-21 02:55:19 +0000

    comment expanding/collapsing textarea update

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.post("/register", function(req, res){
             return res.render("register", {error: err.message});
         }
         passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Welcoeme to Yelpcamp " + user.username);
+            req.flash("success", "Welcoeme to Yelpcamp " + user.firstname);
             res.redirect("/campgrounds");
         });
     });
